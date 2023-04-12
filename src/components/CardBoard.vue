433ee4c122bfcf977b1d5345ef17988e3b1a06a9
CasanovaRamiro <Casanovaramiroj@gmail.com>
2023-04-12 10:45:18 -0300

    added styles and more functionality to pagination

--- src/components/CardBoard.vue
+++ src/components/CardBoard.vue
@@ -1,19 +1,33 @@
 <template>
-    <h1 class="title">Mexican Food Recipes</h1>
-    <button @click="pageUp">page +1</button>
-    <button @click="pageDown">page -1</button>
     <div class="container">
-        <div v-for="item in listItems.slice(page * itemsPerPage, (page + 1) * itemsPerPage)" class="card">
-            <router-link style="text-decoration: none; color: inherit;"
-                :to="{ name: 'CardDetail', params: { id: item.id } }">
-                <img class="card-image" :src="item.image" />
-                <div class="card-content activator">
-                    <span class="card-title activator grey-text text-darken-4">{{ item.title }}</span>
-                    <p>{{ item.difficulty }}</p>
-                </div>
-            </router-link>
+        <h1 class="title">Mexican Food Recipes</h1>
+        <div class="pagination">
+            <span @click="pageDown">Prev</span>
+            <span v-if="page - 1 > 0" @click="page = page - 2">{{ page - 1 }}</span>
+            <span v-if="page > 0" @click="page = page - 1">{{ page }}</span>
+            <span class="current-page">{{ page + 1 }}</span>
+            <span v-if="page + 1 <= Math.floor(this.listItems.length / this.itemsPerPage)" @click="page = page + 1">{{ page
+                + 2
+            }}</span>
+            <span v-if="page + 2 <= Math.floor(this.listItems.length / this.itemsPerPage)" @click="page = page + 2">{{ page
+                + 3
+            }}</span>
+            <span @click="pageUp">Next</span>
         </div>
 
+        <div class="card-container">
+            <div v-for="item in listItems.slice(page * itemsPerPage, (page + 1) * itemsPerPage)" class="card">
+                <router-link style="text-decoration: none; color: inherit;"
+                    :to="{ name: 'CardDetail', params: { id: item.id } }">
+                    <img class="card-image" :src="item.image" />
+                    <div class="card-content activator">
+                        <span class="card-title activator grey-text text-darken-4">{{ item.title }}</span>
+                        <p>{{ item.difficulty }}</p>
+                    </div>
+                </router-link>
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -25,22 +39,19 @@ export default {
         return {
             listItems: [],
             page: 0,
-            itemsPerPage: 10,
+            itemsPerPage: 12,
+
         }
     },
     methods: {
         pageUp() {
             if (Math.floor(this.listItems.length / this.itemsPerPage) === this.page) return
-            console.log(Math.floor(this.listItems.length / this.itemsPerPage), this.page)
             this.page += 1
-            console.log(this.page)
         },
         pageDown() {
             if (this.page === 0) return
             this.page -= 1
-            console.log(this.page)
-        },
-        async getData() {
+        }, async getData() {
             const options = {
                 method: 'GET',
                 headers: {
@@ -48,6 +59,7 @@ export default {
                     'X-RapidAPI-Host': 'the-mexican-food-db.p.rapidapi.com'
                 }
             };
+
 
             await fetch('https://the-mexican-food-db.p.rapidapi.com/', options)
                 .then(response => response.json())
@@ -71,9 +83,48 @@ $box-shadow-general: 0px 2px 16px 0px rgba(0, 0, 0, 0.75);
 $title: 20px;
 $black-font: rgb(40, 40, 40);
 
-.title {
+.container {
     width: $width-container*0.8;
     margin: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.pagination {
+    display: flex;
+    justify-content: space-around;
+    width: 50%;
+    min-width: 300px;
+    max-width: 400px;
+    padding: $padding-general;
+
+    span {
+        margin: auto;
+        font-weight: 500;
+        border-radius: 30px;
+        padding: 3px 8px;
+        cursor: pointer;
+        box-shadow: 0px 1px 8px 0px rgba(0, 0, 0, 0.75);
+        transition: 0.2s;
+    }
+
+    span:hover {
+        transform: scale(1.05);
+        box-shadow: 0px 1px 10px 0px rgba(0, 0, 0, 0.75);
+
+    }
+
+    .current-page {
+        background-color: $black-font;
+        color: beige;
+    }
+}
+
+.title {
+
+    width: $width-container;
     font-size: 40px;
     text-align: center;
     color: $black-font;
@@ -81,8 +132,7 @@ $black-font: rgb(40, 40, 40);
     padding: $padding-general;
 }
 
-.container {
-    width: $width-container;
+.card-container {
     margin: $margin;
     display: flex;
     flex-wrap: wrap;
@@ -92,6 +142,7 @@ $black-font: rgb(40, 40, 40);
 
     .card {
         width: calc($width-container/7);
+        min-width: 250px;
         overflow: hidden;
         border-radius: 4px;
         box-shadow: $box-shadow-general;
